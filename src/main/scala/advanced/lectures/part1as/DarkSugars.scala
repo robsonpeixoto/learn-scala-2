ab1dbf805f481d0647d396e74a3dd98f57cc3439
Robson Roberto Souza Peixoto <robsonpeixoto@gmail.com>
2023-02-08 17:29:06 -0300

    fix: DarkSugars is now runnable with concrete examples

--- src/main/scala/advanced/lectures/part1as/DarkSugars.scala
+++ src/main/scala/advanced/lectures/part1as/DarkSugars.scala
@@ -14,6 +14,20 @@ object DarkSugars extends App {
     42
   }
 
+  println(s"description $description")
+
+  def singleArgMethodByName(arg: => Int): String = {
+    println("hi")
+    val r = s"$arg little ducks..."
+    println("end")
+    r
+  }
+  val descriptionByName = singleArgMethodByName {
+    println("byName")
+    20
+  }
+  println(s"descriptionByName $descriptionByName")
+
   val aTryInstance = Try { // java's try {...}
     throw new RuntimeException
   }
@@ -40,6 +54,8 @@ object DarkSugars extends App {
 
   val aSweeterThread = new Thread(() => println("sweet, Scala!"))
 
+  val aSweeterRunnable: Runnable = () => println("sweet runnable")
+
   abstract class AnAbstractType {
     def implemented: Int = 23
     def f(a: Int): Unit
@@ -58,11 +74,16 @@ object DarkSugars extends App {
   1 :: 2 :: 3 :: List(4, 5)
   List(4, 5).::(3).::(2).::(1) // equivalent
 
-  class MyStream[T] {
-    def -->:(value: T): MyStream[T] = this // actual implementation here
+  class MyStream[T] private (elements: List[T]) {
+    def this() = this(List[T]())
+    def -->:(value: T): MyStream[T] =
+      new MyStream[T](value :: elements)
+    override def toString(): String = this.elements.toString
   }
 
+  // It works because the method finish with `:`
   val myStream = 1 -->: 2 -->: 3 -->: new MyStream[Int]
+  println(myStream)
 
   // syntax sugar #4: multi-word method naming
 
@@ -75,14 +96,16 @@ object DarkSugars extends App {
 
   // syntax sugar #5: infix types
   class Composite[A, B]
-  val composite: Int Composite String = ???
+  val composite: Int Composite String = new Composite[Int, String]
 
   class -->[A, B]
-  val towards: Int --> String = ???
+  val towards: Int --> String = new -->[Int, String]
 
   // syntax sugar #6: update() is very special, much like apply()
   val anArray = Array(1, 2, 3)
+  println(s"Array before $anArray")
   anArray(2) = 7 // rewritten to anArray.update(2, 7)
+  println(s"Array after $anArray")
   // used in mutable collections
   // remember apply() AND update()!
 
@@ -95,6 +118,8 @@ object DarkSugars extends App {
   }
 
   val aMutableContainer = new Mutable
+  println(s"Mutable before ${aMutableContainer.member}")
   aMutableContainer.member = 42 // rewrittern as aMutableContainer.member_=(42)
+  println(s"Mutable after ${aMutableContainer.member}")
 
 }
